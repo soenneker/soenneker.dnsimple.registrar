144d1d9cfc4a0bfeb7cef403ebe8244d9cb9e47c
Jake Soenneker <jake@soenneker.com>
2026-06-08 11:48:08 -0500

    Update DNSimple generated API types

--- src/Soenneker.DNSimple.Registrar/DNSimpleRegistrarUtil.cs
+++ src/Soenneker.DNSimple.Registrar/DNSimpleRegistrarUtil.cs
@@ -36,21 +36,21 @@ public sealed class DNSimpleRegistrarUtil : IDNSimpleRegistrarUtil
     public async ValueTask<DomainCheckResult?> CheckDomainAvailability(string domain, CancellationToken cancellationToken = default)
     {
         DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
-        CheckGetResponse? response = await client[_accountId].Registrar.Domains[domain].Check.GetAsync(cancellationToken: cancellationToken).NoSync();
+        CheckDomain200Response? response = await client[_accountId].Registrar.Domains[domain].Check.GetAsync(cancellationToken: cancellationToken).NoSync();
         return response?.Data;
     }
 
     public async ValueTask<DomainPrices?> GetDomainPrices(string domain, CancellationToken cancellationToken = default)
     {
         DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
-        PricesGetResponse? response = await client[_accountId].Registrar.Domains[domain].Prices.GetAsync(cancellationToken: cancellationToken).NoSync();
+        GetDomainPrices200Response? response = await client[_accountId].Registrar.Domains[domain].Prices.GetAsync(cancellationToken: cancellationToken).NoSync();
 
         return response?.Data;
     }
 
     public async ValueTask RegisterDomain(string domain, int registrantId, CancellationToken cancellationToken = default)
     {
-        var request = new RegistrationsPostRequestBody
+        var request = new DomainRegisterRequest
         {
             RegistrantId = registrantId,
             AutoRenew = true,
@@ -60,11 +60,11 @@ public sealed class DNSimpleRegistrarUtil : IDNSimpleRegistrarUtil
         await RegisterDomain(domain, request, cancellationToken).NoSync();
     }
 
-    public async ValueTask<DomainRegistration?> RegisterDomain(string domain, RegistrationsPostRequestBody request,
+    public async ValueTask<DomainRegistration?> RegisterDomain(string domain, DomainRegisterRequest request,
         CancellationToken cancellationToken = default)
     {
         DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
-        RegistrationsPostResponse? response = await client[_accountId]
+        RegisterDomain201Response? response = await client[_accountId]
                                                     .Registrar.Domains[domain]
                                                     .Registrations.PostAsync(request, cancellationToken: cancellationToken)
                                                     .NoSync();
@@ -74,7 +74,7 @@ public sealed class DNSimpleRegistrarUtil : IDNSimpleRegistrarUtil
     public async ValueTask<DomainRegistration?> GetDomainRegistration(string domain, int registrationId, CancellationToken cancellationToken = default)
     {
         DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
-        WithDomainregistrationGetResponse? response = await client[_accountId]
+        GetDomainRegistration200Response? response = await client[_accountId]
                                                             .Registrar.Domains[domain]
                                                             .Registrations[registrationId]
                                                             .GetAsync(cancellationToken: cancellationToken)
@@ -82,10 +82,10 @@ public sealed class DNSimpleRegistrarUtil : IDNSimpleRegistrarUtil
         return response?.Data;
     }
 
-    public async ValueTask<DomainTransfer?> TransferDomain(string domain, TransfersPostRequestBody request, CancellationToken cancellationToken = default)
+    public async ValueTask<DomainTransfer?> TransferDomain(string domain, DomainTransferRequest request, CancellationToken cancellationToken = default)
     {
         DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
-        TransfersPostResponse? response = await client[_accountId]
+        TransferDomain201Response? response = await client[_accountId]
                                                 .Registrar.Domains[domain]
                                                 .Transfers.PostAsync(request, cancellationToken: cancellationToken)
                                                 .NoSync();
@@ -95,7 +95,7 @@ public sealed class DNSimpleRegistrarUtil : IDNSimpleRegistrarUtil
     public async ValueTask<DomainTransfer?> GetDomainTransfer(string domain, int transferId, CancellationToken cancellationToken = default)
     {
         DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
-        WithDomaintransferGetResponse? response = await client[_accountId]
+        GetDomainTransfer200Response? response = await client[_accountId]
                                                         .Registrar.Domains[domain]
                                                         .Transfers[transferId]
                                                         .GetAsync(cancellationToken: cancellationToken)
@@ -110,10 +110,10 @@ public sealed class DNSimpleRegistrarUtil : IDNSimpleRegistrarUtil
         return true;
     }
 
-    public async ValueTask<DomainRenewal?> RenewDomain(string domain, RenewalsPostRequestBody request, CancellationToken cancellationToken = default)
+    public async ValueTask<DomainRenewal?> RenewDomain(string domain, DomainRenewRequest request, CancellationToken cancellationToken = default)
     {
         DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
-        RenewalsPostResponse? response = await client[_accountId]
+        DomainRenew201Response? response = await client[_accountId]
                                                .Registrar.Domains[domain]
                                                .Renewals.PostAsync(request, cancellationToken: cancellationToken)
                                                .NoSync();
