90083906dd539310d276cde82ae60b250e850daf
soenneker <jake@soenneker.com>
2026-09-03 22:02:06 -0500

    Add interface inheritdoc references

--- src/Soenneker.DNSimple.Registrar/DNSimpleRegistrarUtil.cs
+++ src/Soenneker.DNSimple.Registrar/DNSimpleRegistrarUtil.cs
@@ -18,6 +18,7 @@ using System.Threading.Tasks;
 
 namespace Soenneker.DNSimple.Registrar;
 
+/// <inheritdoc cref="IDNSimpleRegistrarUtil" />
 public sealed class DNSimpleRegistrarUtil : IDNSimpleRegistrarUtil
 {
     private readonly IDNSimpleOpenApiClientUtil _clientUtil;
